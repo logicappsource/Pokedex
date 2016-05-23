aac08ee30303021b3b35c27cdd0949859790ef39
Patrick <patrick@dfsfs-macbook-pro.local>
2016-05-24 01:31:06 +0200

    Rounded Square

--- pokedex/PokeCell.swift
+++ pokedex/PokeCell.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class PokeCell: UICollectionViewCell {
+    
+    
+    
     @IBOutlet weak var thumbImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
     
     
     var pokemon: Pokemon!
     
+    
+   override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+   required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    
+       //Creating round
+       layer.cornerRadius = 20.0
+        
+    }
+    
     func confiqureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
-        
+        //Set label and text
         nameLbl.text = self.pokemon.name.capitalizedString
         thumbImg.image = UIImage(named: "\(self.pokemon.pokexId)")
         
     }
-}
+    
+    
+
+   }
